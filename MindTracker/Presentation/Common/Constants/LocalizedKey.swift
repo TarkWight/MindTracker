141752d8bec5fabbce85ed74c074023a8fec0cb5
TarkWight <arsentiy.gonda@gmail.com>
2025-02-21 13:11:55 +0700

    Added localization

---
+++ MindTracker/Presentation/Common/Constants/LocalizedKey.swift
@@ -0,0 +1,16 @@
+//
+//  LocalizedKey.swift
+//  MindTracker
+//
+//  Created by Tark Wight on 21.02.2025.
+//
+
+import Foundation
+
+enum LocalizedKey {
+    enum AuthView {
+        static let title = NSLocalizedString("authTitle", comment: "Entry screen label")
+        static let buttonTitle = NSLocalizedString("authButtonTitle", comment: "Entry screen button")
+    }
+}
+
