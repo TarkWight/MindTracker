fdf3dec2b2607c78d3e5f12b14e25b8b0212ec02
TarkWight <arsentiy.gonda@gmail.com>
2025-03-03 20:24:49 +0700

    Setup settings view controller
    -- added section titles

--- MindTracker/Presentation/Scenes/Main/TabBar/Statistics/StatisticsViewController.swift
+++ MindTracker/Presentation/Scenes/Main/TabBar/Statistics/StatisticsViewController.swift
@@ -11,6 +11,16 @@ import UIKit
 final class StatisticsViewController: UIViewController {
     let viewModel: StatisticsViewModel
 
+//    private let titleLabel = UILabel()
+    private let emotionsOverviewLabel = UILabel()
+    private let emotionsByDaysLabel = UILabel()
+    private let frequentEmotionsLabel = UILabel()
+    private let moodOverTimeLabel = UILabel()
+
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        fatalError("Use init(viewModel:) instead")
+    }
+
     init(viewModel: StatisticsViewModel) {
         self.viewModel = viewModel
         super.init(nibName: nil, bundle: nil)
@@ -22,10 +32,58 @@ final class StatisticsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemBackground
-        title = "Statistics"
+        view.backgroundColor = viewModel.backgroundColor
+
+//        setupNavigationBar()
+        setupUI()
+        setupConstraints()
     }
-    
+
+//    private func setupNavigationBar() {
+//        titleLabel.text = viewModel.screenTitle
+//        titleLabel.font = viewModel.titleFont
+//        titleLabel.textColor = viewModel.titleColor
+//
+//        navigationItem.titleView = titleLabel
+//    }
+
+    private func setupUI() {
+        let labels = [
+            emotionsOverviewLabel,
+            emotionsByDaysLabel,
+            frequentEmotionsLabel,
+            moodOverTimeLabel
+        ]
+
+        labels.forEach {
+            $0.font = viewModel.sectionFont
+            $0.textColor = viewModel.sectionTextColor
+            $0.translatesAutoresizingMaskIntoConstraints = false
+            view.addSubview($0)
+        }
+
+        emotionsOverviewLabel.text = viewModel.emotionsOverviewTitle
+        emotionsByDaysLabel.text = viewModel.emotionsByDaysTitle
+        frequentEmotionsLabel.text = viewModel.frequentEmotionsTitle
+        moodOverTimeLabel.text = viewModel.moodOverTimeTitle
+    }
+
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            emotionsOverviewLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 24),
+            emotionsOverviewLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+
+            emotionsByDaysLabel.topAnchor.constraint(equalTo: emotionsOverviewLabel.bottomAnchor, constant: 24),
+            emotionsByDaysLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+
+            frequentEmotionsLabel.topAnchor.constraint(equalTo: emotionsByDaysLabel.bottomAnchor, constant: 24),
+            frequentEmotionsLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+
+            moodOverTimeLabel.topAnchor.constraint(equalTo: frequentEmotionsLabel.bottomAnchor, constant: 24),
+            moodOverTimeLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+        ])
+    }
+
     deinit {
         ConsoleLogger.classDeInitialized()
     }
--- MindTracker/Presentation/Scenes/Main/TabBar/Statistics/StatisticsViewModel.swift
+++ MindTracker/Presentation/Scenes/Main/TabBar/Statistics/StatisticsViewModel.swift
@@ -7,18 +7,29 @@
 
 
 import Foundation
+import UIKit
 
 @MainActor
 final class StatisticsViewModel: ViewModel {
-     weak var coordinator: StatisticsCoordinatorProtocol?
+    weak var coordinator: StatisticsCoordinatorProtocol?
+
+    let backgroundColor = UITheme.Colors.background
+
+    let titleColor = UITheme.Colors.appWhite
+
+    let sectionFont = UITheme.Font.StatisticsScene.title
+    let sectionTextColor = UITheme.Colors.appWhite
+
+    let emotionsOverviewTitle = LocalizedKey.Statistics.emotionsOverview
+    let emotionsByDaysTitle = LocalizedKey.Statistics.emotionsByDays
+    let frequentEmotionsTitle = LocalizedKey.Statistics.frequentEmotions
+    let moodOverTimeTitle = LocalizedKey.Statistics.moodOverTime
 
     init(coordinator: StatisticsCoordinatorProtocol) {
         self.coordinator = coordinator
     }
 
-    func handle(_ event: Event) {
-        // Пока нет событий для обработки
-    }
+    func handle(_ event: Event) {}
 }
 
 extension StatisticsViewModel {
