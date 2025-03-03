96bf85725a7ba8a52ed244ddaae5f5a776b52f92
TarkWight <arsentiy.gonda@gmail.com>
2025-03-03 19:09:03 +0700

    Setup emotion settings view

--- MindTracker/Presentation/Scenes/Main/TabBar/Common/NoteSettings/SaveNoteViewModel.swift
+++ MindTracker/Presentation/Scenes/Main/TabBar/Common/NoteSettings/SaveNoteViewModel.swift
@@ -5,30 +5,91 @@
 //  Created by Tark Wight on 23.02.2025.
 //
 
-
 import Foundation
+import UIKit
 
 final class SaveNoteViewModel: ViewModel {
     weak var coordinator: SaveNoteCoordinatorProtocol?
 
+    private var emotion: EmotionCardModel
+    
+    let labelsFont = UITheme.Font.SaveNote.label
+    let labelsColor = UITheme.Colors.appWhite
+    let activityLabel: String = LocalizedKey.SaveNote.activity
+    let peopleLabel: String = LocalizedKey.SaveNote.people
+    let locationLabel: String = LocalizedKey.SaveNote.location
+     var selectedActivityTags: [String]
+     var selectedPeopleTags: [String]
+     var selectedLocationTags: [String]
+
+    var onDataUpdated: (([String], [String], [String]) -> Void)?
+
+    var title: String { LocalizedKey.SaveNote.title }
+    var titleFont: UIFont { UITheme.Font.SaveNote.title }
+    var titleColor: UIColor { UITheme.Colors.appWhite }
+
+    var saveButtonText: String { LocalizedKey.SaveNote.saveButton }
+    var saveButtonColor: UIColor { UITheme.Colors.appWhite }
+    var saveButtonTextColor: UIColor { .black }
+    var saveButtonFont: UIFont { UITheme.Font.SaveNote.saveButton }
+    var saveButtonCornerRadius: CGFloat { 20 }
+
     init(coordinator: SaveNoteCoordinatorProtocol) {
         self.coordinator = coordinator
+        self.emotion = Self.getRandomEmotion()
+
+        self.selectedActivityTags = MockTagsData.activityTags
+        self.selectedPeopleTags = MockTagsData.peopleTags
+        self.selectedLocationTags = MockTagsData.locationTags
+    }
+
+    func getEmotionMock() -> EmotionCardModel {
+        return emotion
     }
 
     func handle(_ event: Event) {
         switch event {
         case .saveNote:
             saveNote()
+        case .dismiss:
+            coordinator?.coordinatorDidFinish()
+        case .updateTags(let type, let tags):
+            updateTags(type: type, tags: tags)
         }
+    }
+
+    private func saveNote() {
+        coordinator?.saveNote()
+    }
+
+    private func updateTags(type: TagType, tags: [String]) {
+        switch type {
+        case .activity:
+            selectedActivityTags = tags
+        case .people:
+            selectedPeopleTags = tags
+        case .location:
+            selectedLocationTags = tags
+        }
+        onDataUpdated?(selectedActivityTags, selectedPeopleTags, selectedLocationTags)
+    }
+
+    private static func getRandomEmotion() -> EmotionCardModel {
+        let randomEmotion = MockEmotionsData.getMockData(for: .five).randomElement() ?? EmotionCardModel(type: .calmness, date: Date())
+        return EmotionCardModel(type: randomEmotion.type, date: randomEmotion.date)
     }
 }
 
 extension SaveNoteViewModel {
     enum Event {
         case saveNote
+        case dismiss
+        case updateTags(type: TagType, tags: [String])
     }
-    
-    private func saveNote() {
-        coordinator?.saveNote()
+
+    enum TagType {
+        case activity
+        case people
+        case location
     }
 }
