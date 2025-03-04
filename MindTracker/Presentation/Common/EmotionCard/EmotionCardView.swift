7af5092c3a7a1edc2250cfcdd023786477b6a847
TarkWight <arsentiy.gonda@gmail.com>
2025-03-04 14:04:43 +0700

    Fixed
    -- emotion card view

--- MindTracker/Presentation/Common/EmotionCard/EmotionCardView.swift
+++ MindTracker/Presentation/Common/EmotionCard/EmotionCardView.swift
@@ -28,11 +28,15 @@ final class EmotionCardView: UIView {
         fatalError("init(coder:) has not been implemented")
     }
     
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        applyRadialGradient(for: emotionLabel.textColor)
+    }
+    
     private func setupUI() {
         translatesAutoresizingMaskIntoConstraints = false
         layer.cornerRadius = 16
         clipsToBounds = true
-        
         backgroundColor = UITheme.Colors.appGray
         
         gradientOverlay.translatesAutoresizingMaskIntoConstraints = false
@@ -58,7 +62,6 @@ final class EmotionCardView: UIView {
         addSubview(emotionIcon)
 
         NSLayoutConstraint.activate([
-//            widthAnchor.constraint(lessThanOrEqualToConstant: 364),
             heightAnchor.constraint(equalToConstant: 158),
 
             gradientOverlay.topAnchor.constraint(equalTo: topAnchor),
@@ -68,17 +71,17 @@ final class EmotionCardView: UIView {
 
             timeLabel.topAnchor.constraint(equalTo: topAnchor, constant: 16),
             timeLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16),
-            
-            feelingLabel.topAnchor.constraint(equalTo: timeLabel.bottomAnchor, constant: 8),
-            feelingLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16),
-            
-            emotionLabel.topAnchor.constraint(equalTo: feelingLabel.bottomAnchor, constant: 4),
-            emotionLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16),
-            
+
             emotionIcon.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -16),
             emotionIcon.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -16),
             emotionIcon.widthAnchor.constraint(equalToConstant: 60),
-            emotionIcon.heightAnchor.constraint(equalToConstant: 60)
+            emotionIcon.heightAnchor.constraint(equalToConstant: 60),
+
+            emotionLabel.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -16),
+            emotionLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16),
+            
+            feelingLabel.bottomAnchor.constraint(equalTo: emotionLabel.topAnchor, constant: -4),
+            feelingLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16)
         ])
     }
     
@@ -88,15 +91,22 @@ final class EmotionCardView: UIView {
         emotionLabel.textColor = emotion.color
         emotionIcon.image = emotion.icon
         
-        applyGradient(for: emotion.color)
+        applyRadialGradient(for: emotion.color)
     }
-    
-    private func applyGradient(for color: UIColor) {
+
+    private func applyRadialGradient(for color: UIColor) {
         let gradientLayer = CAGradientLayer()
         gradientLayer.frame = bounds
-        gradientLayer.colors = [color.withAlphaComponent(1).cgColor, color.withAlphaComponent(0).cgColor]
-        gradientLayer.startPoint = CGPoint(x: 0.5, y: 0)
-        gradientLayer.endPoint = CGPoint(x: 0.5, y: 1)
+        gradientLayer.colors = [
+            color.withAlphaComponent(0.4).cgColor,
+            color.withAlphaComponent(0).cgColor
+        ]
+        gradientLayer.startPoint = CGPoint(x: 1.2, y: -0.2)
+        gradientLayer.endPoint = CGPoint(x: -0.2, y: 2)
+
+        gradientLayer.type = .radial
+
+        gradientOverlay.layer.sublayers?.removeAll()
         gradientOverlay.layer.insertSublayer(gradientLayer, at: 0)
     }
     
--- MindTracker/Presentation/Common/JournalStatusView/JournalStatsView.swift
+++ MindTracker/Presentation/Common/JournalStatusView/JournalStatsView.swift
@@ -11,10 +11,10 @@ import UIKit
 final class JournalStatsView: UIView {
     
     private let stackView = UIStackView()
-    private let totalRecordsLabel = UILabel()
-    private let perDayRecordsLabel = UILabel()
-    private let streakLabel = UILabel()
-    
+    private let totalRecordsView = JournalStatsBadgeView()
+    private let perDayRecordsView = JournalStatsBadgeView()
+    private let streakView = JournalStatsBadgeView()
+
     init() {
         super.init(frame: .zero)
         setupUI()
@@ -25,20 +25,12 @@ final class JournalStatsView: UIView {
     }
     
     private func setupUI() {
-        backgroundColor = .darkGray
-        layer.cornerRadius = 10
-        clipsToBounds = true
-
         stackView.axis = .horizontal
         stackView.alignment = .fill
-        stackView.distribution = .fillEqually
         stackView.spacing = 8
+        stackView.distribution = .equalSpacing
 
-        [totalRecordsLabel, perDayRecordsLabel, streakLabel].forEach { label in
-            label.font = .systemFont(ofSize: 14, weight: .medium)
-            label.textColor = .white
-            stackView.addArrangedSubview(label)
-        }
+        [totalRecordsView, perDayRecordsView, streakView].forEach { stackView.addArrangedSubview($0) }
 
         addSubview(stackView)
         stackView.translatesAutoresizingMaskIntoConstraints = false
@@ -47,16 +39,55 @@ final class JournalStatsView: UIView {
             heightAnchor.constraint(equalToConstant: 32),
             widthAnchor.constraint(equalToConstant: 364),
 
-            stackView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 11.5),
-            stackView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -11.5),
-            stackView.topAnchor.constraint(equalTo: topAnchor, constant: 8),
-            stackView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -8)
+            stackView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            stackView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            stackView.topAnchor.constraint(equalTo: topAnchor),
+            stackView.bottomAnchor.constraint(equalTo: bottomAnchor)
         ])
     }
 
     func updateLabels(totalRecords: String, perDayRecords: String, streakDays: String) {
-        totalRecordsLabel.text = totalRecords
-        perDayRecordsLabel.text = perDayRecords
-        streakLabel.text = streakDays
+        totalRecordsView.setText(totalRecords)
+        perDayRecordsView.setText(perDayRecords)
+        streakView.setText(streakDays)
+    }
+}
+
+final class JournalStatsBadgeView: UIView {
+    
+    private let label = UILabel()
+
+    init() {
+        super.init(frame: .zero)
+        setupUI()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    private func setupUI() {
+        backgroundColor = UIColor.darkGray
+        layer.cornerRadius = 16
+        clipsToBounds = true
+
+        label.font = .systemFont(ofSize: 14, weight: .medium)
+        label.textColor = .white
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+
+        addSubview(label)
+
+        NSLayoutConstraint.activate([
+            heightAnchor.constraint(equalToConstant: 32),
+            label.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 12),
+            label.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -12),
+            label.topAnchor.constraint(equalTo: topAnchor, constant: 8),
+            label.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -8)
+        ])
+    }
+
+    func setText(_ text: String) {
+        label.text = text
     }
 }
