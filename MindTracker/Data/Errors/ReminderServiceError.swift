c4ca6e7235d7ac8af26821d6a54502d6fe1377e6
TarkWight <arsentiy.gonda@gmail.com>
2025-05-09 19:55:06 +0700

    Added settings services error enums
    -- avatar service error
    -- reminder service error

---
+++ MindTracker/Data/Errors/ReminderServiceError.swift
@@ -0,0 +1,12 @@
+//
+//  ReminderServiceError.swift
+//  MindTracker
+//
+//  Created by Tark Wight on 06.05.2025.
+//
+
+import Foundation
+
+enum ReminderServiceError: Error {
+    case reminderNotFound
+}
