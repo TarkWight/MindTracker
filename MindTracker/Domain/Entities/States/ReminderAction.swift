6c7f5a1ece681b8678c9a6ce5d11e8446eada3bd
TarkWight <arsentiy.gonda@gmail.com>
2025-05-11 02:01:46 +0700

    Added Reminder action state
    -- create
    -- update by id

---
+++ MindTracker/Domain/Entities/States/ReminderAction.swift
@@ -0,0 +1,14 @@
+//
+//  ReminderAction.swift
+//  MindTracker
+//
+//  Created by Tark Wight on 11.05.2025.
+//
+
+import Foundation
+
+enum ReminderAction {
+    case create
+    case update(UUID)
+}
+
