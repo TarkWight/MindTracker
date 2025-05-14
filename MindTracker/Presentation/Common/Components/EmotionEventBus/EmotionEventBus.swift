0d2000b7eb82f5c429cf9c965d95305bb0fe63db
TarkWight <arsentiy.gonda@gmail.com>
2025-05-15 01:59:39 +0700

    Added emotion event bus
    -- shared listener to update or add emotion

---
+++ MindTracker/Presentation/Common/Components/EmotionEventBus/EmotionEventBus.swift
@@ -0,0 +1,21 @@
+//
+//  EmotionEventBus.swift
+//  MindTracker
+//
+//  Created by Tark Wight on 15.05.2025.
+//
+
+import Combine
+
+enum EmotionEvent {
+    case added(EmotionCard)
+    case updated(EmotionCard)
+}
+
+@MainActor
+final class EmotionEventBus {
+     static let shared = EmotionEventBus()
+    private init() {}
+
+    let emotionPublisher = PassthroughSubject<EmotionEvent, Never>()
+}
