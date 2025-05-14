3128f0c9a73f06198b5c7172ddb77cc72bc0dab7
TarkWight <arsentiy.gonda@gmail.com>
2025-05-14 19:57:02 +0700

    Update vc
    -- added frequentEmotionsView

--- MindTracker/Presentation/Scenes/Main/TabBar/Statistics/View/StatisticsViewController.swift
+++ MindTracker/Presentation/Scenes/Main/TabBar/Statistics/View/StatisticsViewController.swift
@@ -26,7 +26,7 @@ final class StatisticsViewController: UIViewController, UIScrollViewDelegate {
     private let recordsLabel = UILabel()
     private let emotionOverviewView = EmotionOverviewView()
     private var emotionsByDaysView: EmotionsByDaysView
-//    private var frequentEmotionsView = FrequentEmotionsView()
+    private var frequentEmotionsView = FrequentEmotionsView()
 
     // MARK: - Init
 
@@ -175,10 +175,19 @@ final class StatisticsViewController: UIViewController, UIScrollViewDelegate {
         label.translatesAutoresizingMaskIntoConstraints = false
         section.addSubview(label)
 
+        frequentEmotionsView.translatesAutoresizingMaskIntoConstraints = false
+        section.addSubview(frequentEmotionsView)
+
+
         NSLayoutConstraint.activate([
             label.topAnchor.constraint(equalTo: section.topAnchor, constant: 24),
             label.leadingAnchor.constraint(equalTo: section.leadingAnchor, constant: 24),
-            label.trailingAnchor.constraint(equalTo: section.trailingAnchor, constant: -24)
+            label.trailingAnchor.constraint(equalTo: section.trailingAnchor, constant: -24),
+
+            frequentEmotionsView.topAnchor.constraint(equalTo: label.bottomAnchor, constant: 24),
+            frequentEmotionsView.leadingAnchor.constraint(equalTo: section.leadingAnchor, constant: 16),
+            frequentEmotionsView.trailingAnchor.constraint(equalTo: section.trailingAnchor, constant: -16),
+            frequentEmotionsView.bottomAnchor.constraint(equalTo: section.bottomAnchor, constant: -24),
         ])
 
         sectionViews.append(section)
@@ -354,62 +363,6 @@ final class StatisticsViewController: UIViewController, UIScrollViewDelegate {
         }
     }
 
-    // MARK: - UI Setup
-
-//    private func setupUI() {
-//        scrollView.translatesAutoresizingMaskIntoConstraints = false
-//        scrollView.alwaysBounceVertical = true
-//        view.addSubview(scrollView)
-//
-//        contentView.translatesAutoresizingMaskIntoConstraints = false
-//        scrollView.addSubview(contentView)
-//
-//        titleLabel.text = viewModel.emotionsOverviewTitle
-//        titleLabel.font = viewModel.sectionFont
-//        titleLabel.textColor = viewModel.sectionTextColor
-//        titleLabel.numberOfLines = 2
-//        titleLabel.textAlignment = .left
-//        titleLabel.translatesAutoresizingMaskIntoConstraints = false
-//        contentView.addSubview(titleLabel)
-//
-//
-
-//
-
-//
-//        frequentEmotionsView.translatesAutoresizingMaskIntoConstraints = false
-//        contentView.addSubview(frequentEmotionsView)
-//
-//        NSLayoutConstraint.activate([
-//
-//            scrollView.topAnchor.constraint(equalTo: weekFilterView.bottomAnchor, constant: 8),
-//            scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-//            scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-//            scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
-//
-//            contentView.topAnchor.constraint(equalTo: scrollView.topAnchor),
-//            contentView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor),
-//            contentView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor),
-//            contentView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
-//            contentView.widthAnchor.constraint(equalTo: scrollView.widthAnchor),
-//
-//            titleLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 24),
-//            titleLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 24),
-//            titleLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -24),
-//
-
-//            chartView.heightAnchor.constraint(equalToConstant: 580),
-//            chartView.heightAnchor.constraint(equalTo: contentView.heightAnchor, multiplier: 0.4),
-
-
-//
-//            frequentEmotionsView.topAnchor.constraint(equalTo: emotionsByDaysView.bottomAnchor, constant: 24),
-//            frequentEmotionsView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16),
-//            frequentEmotionsView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16),
-//            frequentEmotionsView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -24),
-//        ])
-//    }
-
     // MARK: - Bindings
 
     private func setupBindings() {
@@ -422,9 +375,9 @@ final class StatisticsViewController: UIViewController, UIScrollViewDelegate {
             self?.updateUI(with: emotionsData, totalRecords: totalRecords)
         }
 
-//        viewModel.onFrequentEmotionsUpdated = { [weak self] frequentEmotions in
-//            self?.frequentEmotionsView.configure(with: frequentEmotions)
-//        }
+        viewModel.onFrequentEmotionsUpdated = { [weak self] frequentEmotions in
+            self?.frequentEmotionsView.configure(with: frequentEmotions)
+        }
     }
 
     private func updateUI(with emotionsData: [EmotionCategory: Int], totalRecords: Int) {
