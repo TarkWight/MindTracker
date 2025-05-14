b294ad88bb3edfc15693851063b42d181f9e1456
TarkWight <arsentiy.gonda@gmail.com>
2025-05-15 05:33:50 +0700

    Added tag storage service
    -- protocol
    -- protocol impl

---
+++ MindTracker/Data/Services/TagService/TagStorageServiceProtocol.swift
@@ -0,0 +1,16 @@
+//
+//  TagStorageServiceProtocol.swift
+//  MindTracker
+//
+//  Created by Tark Wight on 15.05.2025.
+//
+
+import Foundation
+
+protocol TagStorageServiceProtocol: Sendable {
+    func availableTags(for type: TagType) async throws -> [String]
+    func addTag(_ name: String, for type: TagType) async throws
+    func removeTag(_ name: String, from type: TagType) async throws
+    func fetchAllTags() async throws -> EmotionTags
+    func seedDefaultTagsIfNeeded() async throws
+}
