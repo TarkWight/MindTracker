53a93cd39d76ae8225be24f6e7b6ff320fb9fa99
TarkWight <arsentiy.gonda@gmail.com>
2025-05-14 21:06:17 +0700

    Added emotion storage service protocol

---
+++ MindTracker/Data/Services/EmotionStorageService/EmotionStorageService.swift
@@ -0,0 +1,62 @@
+//
+//  EmotionStorageService.swift
+//  MindTracker
+//
+//  Created by Tark Wight on 25.04.2025.
+//
+
+import Foundation
+@preconcurrency import CoreData
+
+final class EmotionStorageService: EmotionStorageServiceProtocol {
+
+    private let context: NSManagedObjectContext
+    private let mapper: EmotionMapperProtocol
+
+    init(context: NSManagedObjectContext, mapper: EmotionMapperProtocol) {
+        self.context = context
+        self.mapper = mapper
+    }
+
+    func saveEmotion(_ emotion: EmotionCardModel) async throws {
+        await context.perform {
+            _ = self.mapper.toEntity(from: emotion, in: self.context)
+        }
+        try await contextSave()
+    }
+
+    func fetchEmotions() async throws -> [EmotionCardModel] {
+        try await context.perform {
+            let request = EmotionEntity.typedFetchRequest
+            let entities = try self.context.fetch(request)
+            return entities.map { self.mapper.toDomain(from: $0) }
+        }
+    }
+
+    func deleteEmotion(by id: UUID) async throws {
+        try await context.perform {
+            let request = EmotionEntity.typedFetchRequest
+            request.predicate = NSPredicate(format: "id == %@", id as CVarArg)
+            let emotions = try self.context.fetch(request)
+            emotions.forEach { self.context.delete($0) }
+        }
+        try await contextSave()
+    }
+
+    func deleteAllEmotions() async throws {
+        try await context.perform {
+            let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "EmotionEntity")
+            let deleteRequest = NSBatchDeleteRequest(fetchRequest: fetchRequest)
+            try self.context.execute(deleteRequest)
+        }
+        try await contextSave()
+    }
+
+    private func contextSave() async throws {
+        try await context.perform {
+            if self.context.hasChanges {
+                try self.context.save()
+            }
+        }
+    }
+}
