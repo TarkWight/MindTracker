91105e08ab1598fe21990b3522d7c3e6a13c4cc0
TarkWight <arsentiy.gonda@gmail.com>
2025-05-14 20:55:44 +0700

    Added core data layer
    -- added app storage
    -- added emotion entity
    -- added emotion tag entity

---
+++ MindTracker/Data/CoreData/Models/EmotionEntity.swift
@@ -0,0 +1,25 @@
+//
+//  EmotionEntity.swift
+//  MindTracker
+//
+//  Created by Tark Wight on 25.04.2025.
+//
+
+import Foundation
+import CoreData
+
+@objc(EmotionEntity)
+final class EmotionEntity: NSManagedObject {
+    @NSManaged var id: UUID
+    @NSManaged var typeRaw: String
+    @NSManaged var timestamp: Date
+    @NSManaged var tagsActivity: Set<EmotionTagEntity>?
+    @NSManaged var tagsPeople: Set<EmotionTagEntity>?
+    @NSManaged var tagsLocation: Set<EmotionTagEntity>?
+}
+
+extension EmotionEntity {
+    static var typedFetchRequest: NSFetchRequest<EmotionEntity> {
+        NSFetchRequest<EmotionEntity>(entityName: "EmotionEntity")
+    }
+}
