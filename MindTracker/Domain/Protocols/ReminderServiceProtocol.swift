b861db4313b03830ce0a891840dd3d46e8fde190
TarkWight <arsentiy.gonda@gmail.com>
2025-05-24 06:15:48 +0700

    Update protocols
    -- replaced
    -- add new protocols
    -- delete unused methods

--- MindTracker/Domain/Protocols/ReminderServiceProtocol.swift
+++ MindTracker/Domain/Protocols/ReminderServiceProtocol.swift
@@ -8,8 +8,8 @@
 import Foundation
 
 protocol ReminderServiceProtocol: Sendable {
-    func loadReminders() async throws -> [Reminder]
+    func fetchReminders() async throws -> [Reminder]
     func createReminder(_ reminder: Reminder) async throws
     func updateReminder(_ reminder: Reminder) async throws
-    func deleteReminder(_ reminder: Reminder) async throws
+    func deleteReminder(by id: UUID) async throws
 }
